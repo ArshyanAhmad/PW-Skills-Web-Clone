8d0567807e4c55a02375d4e386b676727ab59a7e
Arsh <arshahmed235@gmail.com>
2023-04-30 13:10:59 +0530

    staging area

---
+++ assets/js/script.js
@@ -0,0 +1,91 @@
+// #BANNER SWIPER 
+
+let swiper1 = new Swiper(".banner__swiper", {
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+
+});
+
+
+// #WORK SWIPER
+let swiper2 = new Swiper(".work__swiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
+    },
+});
+
+
+// #TESTIMONIAL SWIPER
+let swiper3 = new Swiper(".testimonial__swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    direction: getDirection(),
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+});
+
+// #POPULAR PROGRAM SWIPER
+let swiper4 = new Swiper(".popular_program__swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    direction: getDirection(),
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+});
+
+
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
